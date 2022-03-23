852f1e9977405a35a7bacef71ded353e5a688b67
Cat Goncalves <catgoncalves.dev@gmail.com>
2022-03-22 21:41:18 -0400

    Multiply

---
+++ 8-kyu/multiply.java
@@ -0,0 +1,13 @@
+/**
+* codewars.com/kata/50654ddff44f800200000004/train/java
+* Muliply
+* 
+* Instructions:
+* This code does not execute properly. Try to figure out why.
+ */
+
+ public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}

31916d91188b7020e6e07568f391f83b1c3a4cb8
Cat Goncalves <catgoncalves.dev@gmail.com>
2024-09-10 13:47:38 -0400

    Convert Boolean Values to String Yes or No

---
+++ 8-kyu/convertBooleanValuesToStringYesOrNo.java
@@ -0,0 +1,12 @@
+/**
+ * https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
+ * 
+ * Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ */
+
+public class convertBooleanValuesToStringYesOrNo {
+  public static String boolToWord(boolean b)
+  {
+    return b ? "Yes" : "No";
+  }
+}

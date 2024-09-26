45042974d388ab4cf8639b044a8eb72746d3e58c
Cat Goncalves <catgoncalves.dev@gmail.com>
2024-09-26 07:01:41 -0400

    revision - multiply

--- 8-kyu/multiply.java
+++ 8-kyu/multiply.java
@@ -6,8 +6,8 @@
 * This code does not execute properly. Try to figure out why.
  */
 
- public class Multiply {
-    public static Double multiply(Double a, Double b) {
+ public class multiply {
+    public static Double multiplyTwoDouble(Double a, Double b) {
         return a * b;
     }
 }

c654fb3349d69d464d6c293bdbe41cd41640a249
Cat Goncalves <catgoncalves.dev@gmail.com>
2024-09-27 07:15:54 -0400

    adding kata

---
+++ 8-kyu/Kata.java
@@ -0,0 +1,31 @@
+/**
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+
+Opposite Number
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+ */
+
+ public class Kata {
+  
+  public static int opposite(int number) {
+    return number * -1;
+  }
+}
+
+//or
+
+/**
+    public class Kat2 {
+      
+      public static int opposite(int number) {
+        return -number;
+      }
+    }
+ */

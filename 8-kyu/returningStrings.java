acc0feff1544c99f47f6c7f47b9900645635d15c
Cat Goncalves <catgoncalves.dev@gmail.com>
2024-11-07 14:35:02 -0500

    Returning Strings

---
+++ 8-kyu/returningStrings.java
@@ -0,0 +1,10 @@
+/**
+ * Returning Strings
+ * https://www.codewars.com/kata/55a70521798b14d4750000a4/train/java
+ */
+
+public class returningStrings {
+  public static String greet(String name) {
+    return String.format("Hello, %s how are you doing today?", name);
+  }
+}
